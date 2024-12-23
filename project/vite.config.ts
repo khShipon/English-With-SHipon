5c35de6c883d9de93e8efdd2326a2c11ad3e7767
Onushilon-Genius <onushilongenius@gmail.com>
2024-12-23 18:30:47 +0600

    Deploy to GitHub Pages

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/English-With-SHipon/', // Replace with your repository name
   plugins: [react()],
   root: './', // Ensure index.html is in the root directory
   resolve: {
